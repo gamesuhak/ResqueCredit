8350e848d220d86b73f32025197b019a292d2aaa
lmy6268 <70847610+lmy6268@users.noreply.github.com>
2020-09-22 18:31:48 +0900

    부울이 변화할떄 마다 화면에 해당 키 값의 부울값 출력

--- SuperMario/Input.c
+++ SuperMario/Input.c
@@ -7,16 +7,109 @@ enum Key { KEY_UP = 72, KEY_DOWN = 80, KEY_LEFT = 75, KEY_RIGHT = 77, KEY_A = 'a
 enum Bool { false = 0, true };//true는 0,false는 1
 struct K {
 	int Up; int Down; int Left; int Right; int A; int B;
-
 };
-struct K Kb[1] = { false,false,false,false,false,false };
+int Cng = 0;
+struct K Kb= { false,false,false,false,false,false };//모든 방향키의 부울값을 false로 초기화
 
-Key KeyPush() {
+void MakeFalse() {
+	switch (Cng) //Cng의 값에 따라 변화할 변수의 값을 지정
+	{
+	case KEY_UP:
+		Kb.Up = false;
+		Cng = false;
+		printf("UP false\n");
+		break;
+	case KEY_DOWN:
+		Kb.Down = false;
+		Cng = false;
+		printf("DOWN false\n");
+		break;
+	case KEY_LEFT:
+		Kb.Left = false;
+		Cng = false;
+		printf("LEFT false\n");
+		break;
+	case KEY_RIGHT:
+		Kb.Right = false;
+		Cng = false;
+		printf("RIGHT false\n");
+		break;
+	case KEY_A:
+		Kb.A = false;
+		Cng = false;
+		printf("A KEY false\n");
+		break;
+	case KEY_B:
+		Kb.B = false;
+		Cng = false;
+		printf("B KEY false\n");
+		break;
+	}
+
+}
+void KeyChange(int a) {//받은 인자에 따라 변경할 구조체 멤버 결정
+
+	switch (a) {
+	case KEY_UP:
+		Kb.Up = true;
+		Cng = KEY_UP;
+		break;
+	case KEY_DOWN:
+		Kb.Down = true;
+		Cng = KEY_DOWN;
+		break;
+	case KEY_LEFT:
+		Kb.Left = true;
+		Cng = KEY_DOWN;
+		break;
+	case KEY_RIGHT:
+		Kb.Right = true;
+		Cng = KEY_DOWN;
+		break;
+	case KEY_A:
+		Kb.A = true;
+		Cng = KEY_A;
+		break;
+	case KEY_B:
+		Kb.B = true;
+		Cng = KEY_B;
+		break;
+	}
+}
+void move() {
+	if (Kb.Up == true) {
+		printf("UP true \n");
+		MakeFalse();
+	}
+	if (Kb.Down == true) {
+		printf("DOWN true\n");
+		MakeFalse();
+	}
+	if (Kb.Left == true) {
+		printf("LEFT true\n");
+		MakeFalse();
+	}
+	if (Kb.Right == true) {
+		printf("RIHGT true\n");
+		MakeFalse();
+	}
+	if (Kb.A == true) {
+		printf("A KEY true\n");
+		MakeFalse();
+	}
+	else if (Kb.B == true) {
+		printf("B KEY true\n");
+		MakeFalse();
+	}
+
+
+}
+void KeyPush() {
 	int Pkey = _getch();
 	//방향키입력받기
 	if (Pkey == 0 || Pkey == 224)
 	{
-		return (Key)KeyPush();
+		KeyPush();
 	}
 	if (Pkey == KEY_UP || Pkey == KEY_DOWN || Pkey == KEY_LEFT || Pkey == KEY_RIGHT)
 	{
@@ -29,32 +122,49 @@ Key KeyPush() {
 		else if (Pkey == KEY_RIGHT)
 			KeyChange(KEY_RIGHT);
 	}
-	if (Pkey == 'a' || Pkey == 'A')
-		KeyChange('a');
-	else if (Pkey == 'b' || Pkey == 'B')
-		KeyChange('b');
+	if (Pkey == KEY_A || Pkey == 'A')
+		KeyChange(KEY_A);
+	else if (Pkey == KEY_B || Pkey == 'B')
+		KeyChange(KEY_B);
 }
 
-void KeyChange() {
 
-}
-
-void RcvSgn() {
-	int i;
-	for (i = 0; i < 6; i++)
-	{
-		if (Kb[i] == true)
+/*void Check() {
+	if (Kb.Up == true) {
+		move(KEY_UP);
+		
 	}
-}
+	if (Kb.Down == true) {
+		move(KEY_DOWN);
+		MakeFalse();
+	}
+	if (Kb.Left == true) {
+		move(KEY_LEFT); 
+		MakeFalse();
+	}
+	if (Kb.Right == true) {
+		move(KEY_RIGHT);
+		MakeFalse();
+	}
+	if (Kb.A == true) {
+		move(KEY_A);
+		MakeFalse();
+	}
+	else if(Kb.B== true) {
+		move(KEY_B);
+		MakeFalse();
+	}
+}*/
+
+
 
 int main() {
-	int x = 0, y = 0;
-	struct K key;
-	int Push;
+	
 
 	while (1)
 	{
 		KeyPush();
+		move();
 	};
 	return 0;
 
